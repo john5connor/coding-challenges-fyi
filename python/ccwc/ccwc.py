8d9de7e3f3ee7ec37972c009883a21521d26c70d
John McCarty <mccartyccm120@gmail.com>
2026-04-01 22:18:09 -0400

    handle cl arguments - todo: format total counts in output for more than one file, handle no arguments passed with input file

--- python/ccwc/ccwc.py
+++ python/ccwc/ccwc.py
@@ -11,37 +11,48 @@ def main():
 
     args = parser.parse_args()
 
-    if args.word:
-        process_word_count(args.input_files) 
-    if args.line:
-        process_line_count(args.input_files) 
-    if args.byte:
-        process_byte_count(args.input_files) 
-
-    if not args.word and not args.line and not args.byte:
-        process_line_count(args.input_files) 
-        process_word_count(args.input_files) 
-        process_byte_count(args.input_files) 
+    total_words, total_lines, total_bytes = 0, 0, 0
+    
+    for input_file in args.input_files:
+        if args.word:
+            total_words += process_word_count(input_file) 
+        if args.line:
+            total_lines += process_line_count(input_file) 
+        if args.byte:
+            total_bytes += process_byte_count(input_file) 
         
-def process_word_count(input_files):
-    for file in input_files:
-        with open(file, 'r') as f:
-            word_count = 0
-            for line in f:
-                words = line.split()
-                word_count += len(words)
-        print(word_count, end=" ") 
+        print("", input_file)
 
-def process_line_count(input_files):
-    for file in input_files:
-        with open(file, 'r') as f:
-            line_count = sum(1 for line in f)
-        print(f" {line_count}", end=" ")
+        """
+        if not args.word and not args.line and not args.byte:
+            process_line_count(args.input_files) 
+            process_word_count(args.input_files) 
+            process_byte_count(args.input_files) 
+            """
+        
+def process_word_count(file):
+    print("  ", end="")
+    word_count = 0
+    with open(file, 'r') as f:
+        for line in f:
+            words = line.split()
+            word_count += len(words)
+    print(f"{word_count}", end="")
+    return word_count   
 
-def process_byte_count(input_files):
-    for file in input_files:
-        byte_size = os.path.getsize(file)
-        print(f"{byte_size} {file}", end="\n") 
+def process_line_count(file):
+    print("  ", end="")
+    line_count = 0
+    with open(file, 'r') as f:
+        line_count += sum(1 for line in f)
+    print(f"{line_count}", end="")
+    return line_count
+
+def process_byte_count(file):
+    print("  ", end="")
+    byte_size = os.path.getsize(file)
+    print(f"{byte_size}", end="")
+    return byte_size
 
 if __name__ == "__main__":
     main()
